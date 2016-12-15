1a6a13779596db8b9be2fa703b6e29f3e3219688
Greg Munker <gmunker@gmail.com>
2016-12-15 15:10:27 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { ObjectID } = require('mongodb')
 const { mongoose } = require('./db/mongoose');
@@ -78,6 +79,38 @@ app.delete('/todos/:id', (req, res) => {
 			res.status(400)
 				.send();
 		});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404)
+			.send('Invalid Id')
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date()
+			.getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then((todo) => {
+			if (!todo) {
+				res.status(404)
+					.send('No Todo Found!')
+			}
+
+			res.send({ todo });
+		})
+		.catch((e) => {
+			res.status(400)
+				.send();
+		})
 });
 
 app.listen(port, () => {
