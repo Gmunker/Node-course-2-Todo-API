88cb82a66690ebf522fb2e1679f777f024116aea
Greg Munker <gmunker@gmail.com>
2016-12-20 10:04:27 -0500

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const { User } = require('./../models/user');
 const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 beforeEach(populateUsers);
@@ -193,26 +194,85 @@ describe('PATCH /todos/:id', (done) => {
 	});
 });
 
-describe('GET /users/me', () => {
-	it('should return user if auth', (done) => {
+describe('POST /users', () => {
+	it('should create a user', (done) => {
+		let email = 'example@example.com';
+		let password = '123mnb!';
+
 		request(app)
-			.get('/users/me')
-			.set('x-auth', users[0].tokens[0].token)
+			.post('/users')
+			.send({ email, password })
 			.expect(200)
 			.expect((res) => {
+				expect(res.header['x-auth'])
+					.toExist();
 				expect(res.body._id)
-					.toBe(users[0]._id.toHexString());
+					.toExist();
 				expect(res.body.email)
-					.toBe(users[0].email);
+					.toBe(email);
 			})
+			.end((err) => {
+				if (err) {
+					return done(err)
+				}
+
+				User.findOne({ email })
+					.then((user) => {
+						expect(user)
+							.toExist();
+						expect(user.password)
+							.toNotBe(password);
+						done();
+					})
+			});
+	});
+
+	it('should return validation errors if request invalid', (done) => {
+		let email = 'notAnEmail';
+		let password = '123';
+
+		request(app)
+			.post('/users')
+			.send({ email, password })
+			.expect(400)
+			.end(done)
+	});
+
+	it('should not create user if email in use', (done) => {
+		let email = 'test@example.com';
+		let password = '123mnb!';
+
+		request(app)
+			.post('/users')
+			.send({ email, password })
+			.expect(400)
 			.end(done);
 	});
 
-	it('should return 401 if not auth', (done) => {
-		request(app)
-			.get('/users/me')
-			.set('x-auth', 'abc123')
-			.expect(401)
-			.end(done);
+	describe('GET /users/me', () => {
+		it('should return user if auth', (done) => {
+			request(app)
+				.get('/users/me')
+				.set('x-auth', users[0].tokens[0].token)
+				.expect(200)
+				.expect((res) => {
+					expect(res.body._id)
+						.toBe(users[0]._id.toHexString());
+					expect(res.body.email)
+						.toBe(users[0].email);
+				})
+				.end(done);
+		});
+
+		it('should return 401 if not auth', (done) => {
+			request(app)
+				.get('/users/me')
+				.expect(401)
+				.expect((res) => {
+					expect(res.body)
+						.toEqual({});
+				})
+				.end(done);
+		});
 	});
 });
